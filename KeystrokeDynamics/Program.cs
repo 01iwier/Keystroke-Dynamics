51752ac1651d98fc44f6da91e47a1afa267473fc
Oliwier <wiecko.oli@gmail.com>
2025-01-22 22:01:21 +0000

    Add project files.

---
+++ KeystrokeDynamics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeystrokeDynamics {
+    static class Program {
+        public static string currentUser = "";
+        public static int currentUserID;
+        public const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\database\TestDB.mdf;Integrated Security=True";
+        public const string wordsFilePath = @"..\..\assets\words.txt";
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LaunchScreen());
+        }
+    }
+}
